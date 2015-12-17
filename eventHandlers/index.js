cc6726ea672571b0c45eab674b4aabe0f3f3fa9b
umar-muneer <umar.muneer@careaxiom.com>
2015-12-17 12:01:50 +0500

    corrected promise chain

--- eventHandlers/index.js
+++ eventHandlers/index.js
@@ -7,31 +7,34 @@ var urlJoin = require('url-join');
 var baseUrl = 'https://api.github.com';
 
 var pushEvent = function(payload) {
+  var _getCommitData = function(commit) {
+    return request(urlJoin(baseUrl, 'repos',payload.repository.owner.name, payload.repository.name, 'commits', commit.id))
+      .query({access_token: process.env.GIT_ACCESS_TOKEN})
+      .endAsync()
+      .then(function(result) {
+        var commit = _.pick(result.body.commit, 'author', 'message');
 
-  return new Promise(function(resolve) {
-    var _getCommitData = function(commit) {
-      return request(urlJoin(baseUrl, 'repos',payload.repository.owner.name, payload.repository.name, 'commits', commit.id))
-              .query({access_token: process.env.GIT_ACCESS_TOKEN})
-              .endAsync()
-              .then(function(result) {
-                var commit = _.pick(result.body.commit, 'author', 'message');
-                return _.extend(_.pick(result.body, 'stats', 'files', 'parents'), {
-                  'author': commit.author,
-                  'message': commit.message
-                });
-              });
-    };
-    var isMergeCommit = function(commit) {
-       return commit.parents.length > 1 ? true : false;
-    };
-    var distinctCommits = _.filter(payload.commits, function(commit) {
-      return commit.distinct;
-    });
-    var commitDataPromises = _.map(distinctCommits, function(distinctCommit){
-      return _getCommitData(distinctCommit);
-    });
-    return Promise.all(commitDataPromises).then(resolve);
+        if (isMergeCommit(result.body)) {
+          console.log('Merge commit ignored');
+          return {};
+        }
+        return _.extend(_.pick(result.body, 'stats', 'files', 'parents'), {
+          'author': commit.author,
+          'message': commit.message
+        });
+      });
+  };
+  var isMergeCommit = function(commit) {
+    return commit.parents.length > 1 ? true : false;
+  };
+  var distinctCommits = _.filter(payload.commits, function(commit) {
+    return commit.distinct;
   });
+  var commitDataPromises = _.map(distinctCommits, function(commit) {
+    return _getCommitData(commit);
+  });
+
+  return Promise.all(commitDataPromises);
 };
 
 module.exports.handlePushEvent = function(payload) {
