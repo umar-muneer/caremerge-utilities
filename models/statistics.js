75ac57f26434928994c5146fa0f5983422234111
umar-muneer <umar.muneer@careaxiom.com>
2015-12-18 12:21:39 +0500

    getting team members by team name

--- models/statistics.js
+++ models/statistics.js
@@ -12,18 +12,48 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         // associations can be defined here
       },
-      getTeamMembers: function(teamId) {
-        return request(urlJoin(App.baseUrl, 'teams', teamId, 'members'))
-          .query({access_token: process.env.GIT_ACCESS_TOKEN, per_page:100})
-          .endAsync()
-          .then(function(response) {
+      getCaremergeTeamId: function(organization, teamName) {
+        return request(urlJoin(App.baseUrl, 'orgs', organization, 'teams' ))
+                .query({access_token: process.env.GIT_ACCESS_TOKEN, per_page: 100})
+                .endAsync()
+                .then(function(response) {
+                  var team = _.findWhere(response.body, function(r) {
+                    return r.name === teamName;
+                  });
+                  return team.id;
+                });
+      },
+      getTeamMembers: function(organization, teamName) {
+        return Promise.bind(this).then(function() {
+          return this.getCaremergeTeamId(organization, teamName);  
+        }).then(function(teamId) {
+          return request(urlJoin(App.baseUrl, 'teams', teamId, 'members'))
+            .query({access_token: process.env.GIT_ACCESS_TOKEN, per_page:100})
+            .endAsync()
+          }).then(function(response) {
             return _.map(response.body, function(member) {
               return _.pick(member, 'id', 'login');
-            });
           });
+        });
       },
       calculateTeamMemberStats: function(member, toDate, fromDate) {
-        return Promise.resolve({});
+        var _this = this;
+        var calculateCommitStats = function() {
+          return _this.findAll({
+            where: {
+              author: member.login,
+              type: 'commit'
+            }
+          }).then(function(commitInfo) {
+            console.log(commitInfo);
+          });
+        };
+
+        var calculatePullRequestStats = function() {
+
+        };
+
+
       },
       calculate: function(data) {
         var _this = this;
@@ -33,7 +63,7 @@ module.exports = function(sequelize, DataTypes) {
             throw "to date not specified";
           if (!_.has(data, 'fromDate') || !data.fromDate)
             throw "from date not specified";
-          return _this.getTeamMembers(process.env.TEAM_ID);
+          return _this.getTeamMembers(process.env.CM_GIT_ORGANIZATION, process.env.CM_TEAM_NAME);
         }).then(function(members) {
           console.log('####', members, '####');
         });
