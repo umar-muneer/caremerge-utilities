d263882fa2b0b2c542478764c2a73f8fc4f45ecf
Umar Muneer <umar.muneer@careaxiom.com>
2016-04-10 14:23:59 +0500

    more logging

--- models/dump.js
+++ models/dump.js
@@ -2,6 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var model = sequelize.define('git_dump', {
     data: DataTypes.JSONB
   });
-  console.log(model);
+  console.log('@@@@', 'am i even coming here');
+  console.log('@@@@', model);
   return model; 
 };
